a7bf5fcf196fa097b32789b9f034a0daa77eb324
mervanerdem <mrvnerdem@gmail.com>
2022-04-08 02:21:33 +0300

    Subtract Func

--- Integers/main.go
+++ Integers/main.go
@@ -4,6 +4,10 @@ func Add(a, b int) int {
 	return a + b
 }
 
+func Sub(a, b int) int {
+	return a - b
+}
+
 func main() {
 
 }
--- Integers/main_test.go
+++ Integers/main_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestAdder(t *testing.T) {
-	sum := Add(2, 2)
-	expected := 4
 
-	if sum != expected {
-		t.Errorf("expected: %d but got: %d", expected, sum)
-	}
+	t.Run("Adding test", func(t *testing.T) {
+		sum := Add(2, 2)
+		expected := 4
+
+		if sum != expected {
+			t.Errorf("expected: %d but got: %d", expected, sum)
+		}
+	})
+	t.Run("Sub Test", func(t *testing.T) {
+		sub := Sub(5, 4)
+		expected := 1
+
+		if sub != expected {
+			t.Errorf("expected: %d but got: %d", expected, sub)
+		}
+	})
+
 }
